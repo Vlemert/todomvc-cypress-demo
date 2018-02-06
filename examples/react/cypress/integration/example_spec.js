750811f8908a493f0f920882f1306622dd2237af
Niels Vleeming <niels.vleeming@greenhousegroup.com>
2018-02-06 19:13:03 +0100

    add tests

---
+++ examples/react/cypress/integration/example_spec.js
@@ -0,0 +1,28 @@
+describe('TODO app', function () {
+	it('can create new todos', function () {
+		cy.visit('http://localhost:8080')
+
+		cy.get('[placeholder="What needs to be done?"').as('inputField')
+
+		cy.get('@inputField').type('show the test works{enter}')
+		cy.get('@inputField').type('for real{enter}')
+
+		cy.get('.todo-list').as('todoList');
+		cy.get('@todoList').contains('show the test works')
+		cy.get('@todoList').contains('for real')
+	})
+
+	it('can edit a todo', function () {
+		cy.visit('http://localhost:8080')
+
+		cy.get('[placeholder="What needs to be done?"').as('inputField')
+
+		cy.get('@inputField').type('show the test works{enter}')
+
+		cy.get('.todo-list').as('todoList');
+		cy.get('@todoList').contains('show the test works').dblclick()
+		cy.get('@todoList').get('[value="show the test works"]').type('{selectAll}{backspace}some new text{enter}')
+		cy.get('@todoList').contains('show the test works').should('not.exist')
+		cy.get('@todoList').contains('some new text')
+	})
+})
