4d0a65f5217d6074684c1a2004936da95e834650
Niels Vleeming <niels.vleeming@greenhousegroup.com>
2018-02-06 13:04:59 +0100

    Break test

--- examples/react/cypress/integration/example_spec.js
+++ examples/react/cypress/integration/example_spec.js
@@ -20,7 +20,7 @@ describe('TODO app', function () {
 		cy.get('@inputField').type('show the test works{enter}')
 
 		cy.get('.todo-list').as('todoList');
-		cy.get('@todoList').contains('show the test works').dblclick()
+		// cy.get('@todoList').contains('show the test works').dblclick()
 		cy.get('@todoList').get('[value="show the test works"]').type('{selectAll}{backspace}some new text{enter}')
 		cy.get('@todoList').contains('show the test works').should('not.exist')
 		cy.get('@todoList').contains('some new text')
